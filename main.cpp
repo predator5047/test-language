82be56596fcb50090c34cf29d28ce59c6e5af2e5
Buţiu Alexandru Octavian <predator5047@gmail.com>
2015-03-29 21:15:49 +0300

    Cleaned up main
    Signed-off-by: Buţiu Alexandru Octavian <predator5047@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
-#include "lexer.h"
+#include "parser.h"
 using namespace std;
 
-const string tokid_to_str[] = {
-	"NUMBER", "IDENT", "EQ", "PLUS", "MINUS", "DIV",
-	"MULT", "SEMICOLON", "UNKOWN"
-};
-
 int main() {
-	Lexer l("test.txt");
-
-	Token t = l.NextToken();
-
-	while (t.Id != UNKOWN) {
-		if (t.Id == IDENT) {
-			cout << t.Name << " ";
-		} else if (t.Id == NUMBER) {
-			cout << t.Value << " ";
-		} else {
-			cout << tokid_to_str[t.Id] << " ";
-		}
-		t = l.NextToken();
-	}
-	
-	cout << "\n";
 
 	return 0;
 }
